79a9639961afe3f07f3bf0642c6c46c27e096196
Yan Zhuang <zhuangy@usc.edu>
2012-09-24 21:38:25 -0700

    test3

---
+++ TiltShooting/HelperScene.m
@@ -0,0 +1,14 @@
+//
+//  HelperScene.m
+//  TiltShooting
+//
+//  Created by yan zhuang on 12-9-18.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "HelperScene.h"
+
+
+@implementation HelperScene
+
+@end
