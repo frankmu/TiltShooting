6eacef5ecef2bce2a0b6537c0c0c4434acd311f9
Yirui Zhang <bcp1989@gmail.com>
2012-11-18 16:27:53 -0800

    Change
    Change:
    Target现在有width和height并且已经在init的时候初始化好，ui需要在targetAppear的时候获取width和height
    然后设置CCSprite的scaleX和scaleY从而让CCSprite的大小符合target，比如target的width是20，CCSpr
    ite图片的大小是40，那么scaleX就是0.5
    
    Time消息的通知现在是每个刷新都会通知而不是每秒
    新加bonus接口
    
    Add switch weapon interface
    
    Model will automatically stopped after running out of time
    remove all children after receiving gameFinished
    
    impact 消息被取消
    
    Shaking的支持：
    需要将AppDelegate.h里的window_改为ShakeEnabledUIWindow，将AppDelegate.m里的window_的
    初始化部分改为window_ = [[ShakeEnabledUIWindow
    alloc]……….]后面加window_.isShakeEnabled = YES;

---
+++ TiltShooting/M4A1.h
@@ -0,0 +1,13 @@
+//
+//  M4A1.h
+//  TiltShooting
+//
+//  Created by yirui zhang on 11/13/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface M4A1 : NSObject
+
+@end
---
+++ TiltShooting/M4A1.m
@@ -0,0 +1,13 @@
+//
+//  M4A1.m
+//  TiltShooting
+//
+//  Created by yirui zhang on 11/13/12.
+//
+//
+
+#import "M4A1.h"
+
+@implementation M4A1
+
+@end
