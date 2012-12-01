dffe9b0f31ce5edf71a38b9afe4860433f4da45b
Yirui Zhang <bcp1989@gmail.com>
2012-11-30 23:47:37 -0800

    test

---
+++ TiltShooting/Spider.h
@@ -0,0 +1,13 @@
+//
+//  Spider.h
+//  TiltShooting
+//
+//  Created by yirui zhang on 11/30/12.
+//
+//
+
+#import "Enemy.h"
+
+@interface Spider : Enemy
+
+@end
---
+++ TiltShooting/Spider.m
@@ -0,0 +1,13 @@
+//
+//  Spider.m
+//  TiltShooting
+//
+//  Created by yirui zhang on 11/30/12.
+//
+//
+
+#import "Spider.h"
+
+@implementation Spider
+
+@end
